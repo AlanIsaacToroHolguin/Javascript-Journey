c1705a0a8174b53c7a9ea31ec1a2ecd2e8717d47
AlanIsaacToroHolguin <alanpegazo@gmail.com>
2025-08-09 00:18:33 -0500

    Variables JS

---
+++ 02_Variables/Script.js
@@ -0,0 +1,11 @@
+//VARIABLES
+//A variable is a container that stores a value or reference to be used and manipulated in the code.
+
+// var → old, function/global scope, avoid using
+var x = 10;
+
+// let → modern, block scope, value can change
+let y = 20;
+
+// const → modern, block scope, value cannot change
+const z = 30;
