1b8d27938f0100d3146dafdbddc919425b9c2a25
AlanIsaacToroHolguin <alan.software.arch@gmail.com>
2025-08-10 00:49:30 -0500

    Constants JS

---
+++ 03_Constants/Script.js
@@ -0,0 +1,7 @@
+//Constants
+//A constant is a value that does not change during the execution of a program.
+let name = "Alan";
+const ID_NUMBER = 1234567890; // This is my ID number and it never changes
+
+console.log("The " + name + " ID is: " + ID_NUMBER);
+
