58c474b39f717a2a0c328f4f3d07f9258e6c6192
AlanIsaacToroHolguin <alan.software.arch@gmail.com>
2025-08-24 23:21:45 -0500

    JS Ejerxice 02

---
+++ 06_Ejerxices/02.js
@@ -0,0 +1,17 @@
+/*
+Exercise 1:
+
+We have 7 boxes with 8 cookies each.  
+How many cookies are there in total?  
+Store the result in a variable and display it in the console.
+*/
+
+// Data
+const numberOfBoxes = 7;
+const cookiesPerBox = 8;
+
+// Calculation
+const totalCookies = numberOfBoxes * cookiesPerBox;
+
+// Output
+console.log(`The total number of cookies is: ${totalCookies}`);
