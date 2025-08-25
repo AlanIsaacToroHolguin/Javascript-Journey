ac69a04522960c2777a4e5127c371e6187ba59ff
AlanIsaacToroHolguin <alan.software.arch@gmail.com>
2025-08-24 23:29:17 -0500

    JS Ejerxice 03

---
+++ 06_Ejerxices/03.js
@@ -0,0 +1,17 @@
+/*
+Exercise 1:
+
+How many minutes are there in 7 hours and 22 minutes?  
+Store the result in a variable and display it in the console.
+*/
+
+// Data
+const hours = 7;
+const minutes = 22;
+
+// Calculation
+const totalMinutes = (hours * 60) + minutes;
+
+// Output
+console.log(`In ${hours} hours and ${minutes} minutes, there are ${totalMinutes} minutes.`);
+
