53e8f3ffde5641b16d0c934644c7c2e2996bc9ec
AlanIsaacToroHolguin <alan.software.arch@gmail.com>
2025-08-26 22:47:08 -0500

    JS Ejerxice 07

---
+++ 06_Ejerxices/07.js
@@ -0,0 +1,15 @@
+/*
+Exercise 7:
+
+We have a circle with a diameter of 18 cm.  
+Calculate the radius of the circle and show it in an alert.
+*/
+
+// Data
+const diameter = 18;
+
+// Calculation
+const radius = diameter / 2;
+
+// Output
+alert(`The radius of the circle is ${radius} cm.`);
